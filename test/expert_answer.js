267bc6a743000dc283ed0a7eb4d4b95d39876f36
qianningxu <108269194+Ricardo-u@users.noreply.github.com>
2024-08-17 13:40:10 +0100

    inline-ai-api

---
+++ test/expert_answer.js
@@ -0,0 +1,22 @@
+import { functions } from '../firebaseConfig.js';
+import { httpsCallable } from 'firebase/functions';
+
+// Reference to the callable function
+const testFunction = httpsCallable(functions, 'expert_answer_api')
+
+const data = {
+    "question": "How did China rise up in the 21th century?"
+};
+
+// Convert the data object to a JSON string
+const jsonData = JSON.stringify(data);
+
+testFunction(jsonData)
+    .then(({ data: responseData }) => {
+        console.log('Function call successful:', responseData);
+        // You can handle the response data here, such as updating the UI
+    })
+    .catch(error => {
+        console.error('Error calling the function:', error);
+        // You can handle errors here, such as displaying an error message to the user
+    });
