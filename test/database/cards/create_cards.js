fa43f03e38d318f26ad414b315a858e8a6feaeb5
qianningxu <108269194+Ricardo-u@users.noreply.github.com>
2024-08-23 14:11:28 +0100

    database CURD for 5 entities

--- test/database/cards/create_cards.js
+++ test/database/cards/create_cards.js
@@ -4,10 +4,10 @@ import { httpsCallable } from 'firebase/functions';
 const createCardFunction = httpsCallable(functions, 'create_card');
 
 const data = {
-    "canvas_id": "canvasID1",
-    "card_id": "cardID1",
-    "title": "Card 1",
-    "content": "This is a sample card.",
+    "canvas_id": "0asdf01",
+    "card_id": "sdfgID1",
+    "title": "Carsd 1",
+    "content": "This is a sple sgfdcard.",
     "position": { x: 100, y: 200 },
     "nodes": { north: true, south: true, west: true, east: true }
 };
